334bf2e1964dedab1a42cb5d5cc41d9065f52023
rajivasia <rrrajib.rimal@gmail.com>
2016-05-18 18:52:00 +0545

    For loop added

--- Example/src/com/sajiv/example/Program.java
+++ Example/src/com/sajiv/example/Program.java
@@ -15,8 +15,9 @@ public class Program {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("Hello World");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Hello World " +i);
+        }
     }
-    
+
 }
